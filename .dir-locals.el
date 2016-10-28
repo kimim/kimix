f32c5d7d4cf34ca90f042f8d65fbc4c3c6e54e03
kimim <m@kimi.im>
2016-10-28 20:31:30 +0800

    fix readme and add local clang config

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
 ((nil . ((indent-tabs-mode . t)
          (tab-width . 8)
          (fill-column . 80)))
-         ;; Warn about spaces used for indentation:
-         (c-mode . ((c-file-style . "BSD"))))
+ ;; Warn about spaces used for indentation:
+ (c-mode . ((c-file-style . "BSD"))))
